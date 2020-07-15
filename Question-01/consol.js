50b56fae6d4818b899f10f0d75d12e2e7e9b26d5
SuhailNawazEngg <68140327+SuhailNawazEngg@users.noreply.github.com>
2020-07-15 12:51:58 +0530

    Add files via upload

---
+++ Question-01/consol.js
@@ -0,0 +1,27 @@
+// alert("Click me to Proceed");
+console.log("hey I am a javascript code");
+
+// Comments in javascript
+// Single line comments are written using //
+// Multine Comments are written using /* */
+
+/*console.log("Hello");
+console.log("Hi");
+console.log("Hii");
+console.log("Hy");*/
+
+var a = {name:"Suhail", age:"18", city:"Kolkata"}
+var b = {name:"Nawaz", age:"18", city:"Gaya"}
+var c = {name:"Prince", age:"18", city:"Lucknow"}
+
+console.time('Time taken');
+console.log("Hey")
+console.error("an error occured!");
+console.warn("Hey this is a warning!!");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log({a ,b ,c})
+console.table({a ,b ,c})
+console.log('%c Custom message',''color:blue;')
+console.timeEnd('Time taken');
